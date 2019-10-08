1c2960974a5be01e6bac674ea9fef8341ed03b0c
유용태 <>
2019-10-08 17:58:48 +0900

    evaluate observable

--- 1px.js.new/compile.js
+++ 1px.js.new/compile.js
@@ -1,32 +1,32 @@
 (function() {
 	"use strict";
-
+	
 	const {$module} = require("./1px.module");
 	const {JSContext} = require("./parse");
-
+	
 	/// -----------------------------------------------------------------------
 	/// traverseDOM
 	/// -----------------------------------------------------------------------
 	function traverseDOM(node, callback) {
 		if (!node) return;
-
+		
 		const queue = ("length" in node) ? Array.from(node) : [node];
-
-		while (queue.length) {
+		
+		while(queue.length) {
 			node = queue.shift();
-
+			
 			// Option: Closing,
 			if (typeof node === "function") {
 				node();
 				continue;
 			}
-
+			
 			// Option: Skip children,
 			let ret = callback(node);
 			if (ret === false) {
 				continue;
 			}
-
+			
 			// Traverse ChildNodes
 			if (node.childNodes) {
 				if (typeof ret === "function") queue.unshift(ret);
@@ -34,7 +34,7 @@
 			}
 		}
 	}
-
+	
 	/// -----------------------------------------------------------------------
 	/// ELEMENT_NODE
 	/// -----------------------------------------------------------------------
@@ -44,10 +44,10 @@
 			case "SCRIPT":
 				return false;
 		}
-
+		
 		let ret;
-
-
+		
+		
 		const attributes = Array.from(el.attributes);
 		to = to || el;
 		if (to !== el) {
@@ -55,7 +55,7 @@
 				to.setAttributeNode(attr.cloneNode(true));
 			}
 		}
-
+		
 		/// @NOTE: FORM Submit 방지
 		if (el.tagName === "FORM") {
 			if (!el.hasAttribute("method") && !el.hasAttribute("action")) {
@@ -64,11 +64,11 @@
 					this.dispatchEvent(new CustomEvent("submit"));
 				};
 			}
-
+			
 			// @TODO: validate, keyenter -> submit, etc...
 		}
-
-
+		
+		
 		/// @FIXME:... default template directive
 		/// Default Template Directives
 		let hasTemplateDirective = ["*foreach", "*if", "*else"].some(attrName => {
@@ -76,21 +76,21 @@
 			if (!attr) {
 				return false;
 			}
-
+			
 			const directive = $module.directive.get(attr.nodeName);
 			directive(context, to, attr.nodeValue, attr.nodeName);
 			return true;
 		});
-
+		
 		if (hasTemplateDirective) {
 			return false;
 		}
-
-
+		
+		
 		/// Directive: "is"
 		/// @TODO: is="app-component as b"
 		ret = _createComponentIfDefined(context, to);
-
+		
 		/// Attribute directive
 		for (const attr of attributes) {
 			// /// Custom Directives
@@ -103,8 +103,8 @@
 			// 	}
 			// }]);
 			// if (customDefaultPrevent) continue;
-
-
+			
+			
 			/// Basic Directives
 			if (templateSyntax(context, to, attr, "(", _event, ")")) continue;
 			if (templateSyntax(context, to, attr, "[attr.", _attr, "]")) continue;
@@ -119,25 +119,24 @@
 			if (templateSyntax(context, to, attr, "#", _ref, "")) continue;
 			if (templateSyntax(context, to, attr, ".", _call, ")")) continue;
 		}
-
+		
 		return ret;
 	}
-
-
+	
+	
 	/// @FIXME...
 	function templateSyntax(context, el, attr, start, callback, end) {
-		const name = attr.nodeName;
-		const value = attr.nodeValue;
-
-		if (name.startsWith(start) && name.endsWith(end)) {
-			callback(context, el, value, name.slice(start.length, -end.length));
+		const {nodeName, nodeValue} = attr;
+		
+		if (nodeName.startsWith(start) && nodeName.endsWith(end)) {
+			callback(context, el, nodeValue, name.slice(start.length, -end.length));
 			// el.removeAttributeNode(attr); // @TODO: DEBUG mode
 			return true;
 		}
 	}
-
+	
 	function _createComponentIfDefined(context, el) {
-
+		
 		// let Component = ($module.exist(el.getAttribute("is") && $module.get(el.getAttribute("is")) || el.tagName.toLowerCase());
 		// if (!Component) {
 		// 	return;
@@ -166,15 +165,15 @@
 		//
 		// return context;
 	}
-
-
+	
+	
 	function _visible(context, el, script, prop) {
 		prop = "hidden";
 		context.watch$(script, value => el[prop] = !value);
 	}
-
+	
 	function _visible2(context, el, script, prop) {
-
+		
 		context.watch$(script, value => {
 			if (value) {
 				el.hidden = false;
@@ -184,19 +183,19 @@
 			}
 		});
 	}
-
+	
 	function _prop(context, el, script, prop) {
 		context.watch$(script, value => el[prop] = value);
 	}
-
-
+	
+	
 	// function _getOptions(value) {
 	// 	return options.reduce((o, option) => {
 	// 		o[option] = true;
 	// 		return o;
 	// 	}, Object.create(null));
 	// }
-
+	
 	Event.pipes = {
 		prevent: $ => $.do(e => e.preventDefault()),
 		stop: $ => $.do(e => e.stopPropagation()),
@@ -209,146 +208,146 @@
 		meta: $ => $.filter(e => e.metaKey),
 		cmd: $ => $.filter(e => e.ctrlKey || e.metaKey),
 	};
-
+	
 	function _event(context, el, script, value) {
-
+		
 		let [type, ...options] = value.split("|");
 		let useCapture = options.indexOf("capture") >= 0;
-
+		
 		/// Keyboard Event
 		let keys = [];
 		if (type.startsWith("keydown") || type.startsWith("keypress") || type.startsWith("keyup")) {
 			[type, ...keys] = type.split(".");
 		}
-
+		
 		let event;
 		let o$ = context.on$(el, type, useCapture).do(e => event = e);
-
+		
 		if (keys.length) {
 			o$ = o$.filter(e => {
 				return keys.indexOf(e.key.toLowerCase()) >= 0;
 			});
 		}
-
+		
 		/// Event Pipe
 		options.forEach(pipe => {
 			let handler = Event.pipes[pipe] || Event.pipes["*"];
 			if (!handler) throw new Error(pipe + " is not registered event pipe.");
 			o$ = handler(o$, el);
 		});
-
+		
 		/// Event Handler
 		o$.subscribe(function(event) {
 			context.fork({event, el}).evaluate(script);
 		});
 	}
-
+	
 	/// two-way
 	function _twoway(context, el, script, value) {
-
+		
 		let [prop, eventType, ...options] = value.split(".");
-
+		
 		options = options.reduce((o, option) => {
 			o[option] = true;
 			return o;
 		}, Object.create(null));
-
-
+		
+		
 		context.watch$(script, value => el[prop] = value);
 		context.on$(el, eventType || "input").subscribe(function() {
 			context.assign(script, el[prop]);
 		});
 	}
-
+	
 	function _attr(context, el, script, attr) {
 		context.watch$(script, value => (!value && value !== 0) ? el.removeAttribute(attr) : el.setAttribute(attr, value))
 	}
-
+	
 	function _style(context, el, script, name) {
-
+		
 		let [prop, unit] = name.split(".", 2);
 		unit = unit || "";
-
+		
 		context.watch$(script, value => {
 			switch (unit) {
 				case "url":
 					value = "url('" + encodeURI(value) + "')";
 					break;
-
+				
 				default:
 					value = value + unit;
 					break;
 			}
-
+			
 			el.style[prop] = value;
 		});
 	}
-
+	
 	function _class(context, el, script, name) {
 		context.watch$(script, value => value ? el.classList.add(name) : el.classList.remove(name));
 	}
-
+	
 	function _ref(context, el, script, name) {
 		context.local[name] = el;
 	}
-
+	
 	function _ref2(context, el, script, name) {
 		context.thisObj["$" + name] = el;
 	}
-
+	
 	function _call(context, el, script, name) {
 		context.watch$(script, value => {
 			if (value === true) {
-
+				
 				/// @FIXME:...
 				let doScript = "_tmp." + name + ")";
 				// console.log("_call", el, doScript);
-
+				
 				let _context = context.fork({_tmp: el});
-
+				
 				try {
 					_context.evaluate(doScript);
-
+					
 				} catch (e) {
-
+					
 					console.error(e);
 				}
 			}
 		});
 	}
-
+	
 	function _transition(context, el, script, name) {
-
+		
 		function transitionend() {
 			el.classList.remove(name + "-enter-active");
 			el.classList.remove(name + "-enter");
 			el.classList.remove(name + "-enter-to");
-
+			
 			el.removeEventListener("transitionend", transitionend);
 			el.removeEventListener("transitionend", transitionHideEnd);
 		}
-
+		
 		function transitionHideEnd() {
 			el.classList.remove(name + "-leave-active");
 			el.classList.remove(name + "-leave");
 			el.classList.remove(name + "-leave-to");
 			el.hidden = true;
-
+			
 			el.removeEventListener("transitionend", transitionend);
 			el.removeEventListener("transitionend", transitionHideEnd);
 		}
-
+		
 		context.watch$(script, value => {
 			if (value === undefined) return;
-
+			
 			if (value) {
 				el.removeEventListener("transitionend", transitionend);
 				el.removeEventListener("transitionend", transitionHideEnd);
-
+				
 				el.classList.add(name + "-enter-active");
 				el.classList.add(name + "-enter");
 				el.hidden = false;
-
+				
 				if (parseFloat(window.getComputedStyle(el).transitionDuration)) {
 					el.addEventListener("transitionend", transitionend, false);
 					requestAnimationFrame(() => {
@@ -363,17 +362,17 @@
 			else {
 				el.removeEventListener("transitionend", transitionend);
 				el.removeEventListener("transitionend", transitionHideEnd);
-
+				
 				el.classList.add(name + "-leave-active");
 				el.classList.add(name + "-leave");
 				el.offsetWidth;
-
+				
 				if (parseFloat(window.getComputedStyle(el).transitionDuration)) {
-
-
+					
+					
 					el.addEventListener("transitionend", transitionHideEnd, false);
 					el.addEventListener("transitioncancel", transitionHideEnd, false);
-
+					
 					requestAnimationFrame(() => {
 						el.classList.remove(name + "-leave");
 						el.classList.add(name + "-leave-to");
@@ -385,8 +384,8 @@
 			}
 		});
 	}
-
-
+	
+	
 	/// -----------------------------------------------------------------------
 	/// TEXT_NODE
 	/// -----------------------------------------------------------------------
@@ -396,69 +395,69 @@
 			this.__node.forEach(node => node.remove());
 			delete this.__node;
 		}
-
+		
 		if (value instanceof Node) {
 			this.nodeValue = "";
 			this.__node = Array.from(value.childNodes || [value]).slice();
 			this.before(value);
 			return;
 		}
-
+		
 		this.nodeValue = value === undefined ? "" : value;
 	}
-
+	
 	function compile_text_node(node, context) {
 		let index = node.nodeValue.indexOf("{{");
-
-		while (index >= 0) {
+		
+		while(index >= 0) {
 			node = node.splitText(index);
 			index = node.nodeValue.indexOf("}}");
 			if (index === -1) return;
-
+			
 			let next = node.splitText(index + 2);
 			let script = node.nodeValue.slice(2, -2);
 			node.nodeValue = "";
 			context.watch$(script, _nodeValue.bind(node));
-
+			
 			node = next;
 			index = node.nodeValue.indexOf("{{");
 		}
 	}
-
-
+	
+	
 	/// -----------------------------------------------------------------------
 	/// $compile
 	/// -----------------------------------------------------------------------
 	function $compile(el, context, globalObj, if_template_to) {
 		if_template_to = if_template_to || el;
-
+		
 		if (arguments.length === 1) {
 			context = context || new JSContext(el, globalObj);
 		}
 		if (!(context instanceof JSContext)) {
 			context = new JSContext(context, globalObj);
 		}
-
+		
 		if (el.tagName === "TEMPLATE") {
 			compile_element_node(el, context, if_template_to);
 			el = el.content;
 		}
-
+		
 		traverseDOM(el, node => {
 			// if (node.__compiled) return false;
-
+			
 			switch (node.nodeType) {
 				case Node.ELEMENT_NODE:
 					return compile_element_node(node, context);
-
+				
 				case Node.TEXT_NODE:
 					return compile_text_node(node, context);
 			}
 		});
-
+		
 		return context.thisObj;
 	}
-
+	
 	exports.traverseDOM = traverseDOM;
 	exports.compile_text_node = compile_text_node;
 	exports.$compile = $compile;
